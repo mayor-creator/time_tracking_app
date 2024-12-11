fdbbd91fce3f0709c16698046d99ddd2ec1279a0
Mayor Creator <paulaboagye@outlook.com>
2024-12-11 11:25:10 -0500

    add css style properties

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,30 @@
-import profileImage from "../assets/images/image-jeremy.png";
 import { Button } from "./Button.jsx";
+import profileImage from "../assets/images/image-jeremy.png";
+import styles from "../components/Profile.module.css";
+
 export function Profile() {
   return (
     <>
-      <div>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.profile_container}>
+          <img
+            className={styles.profile_image}
+            src={profileImage}
+            alt="a person listening to music"
+            height={64}
+            width={64}
+          />
           <div>
-            <img src={profileImage} alt="a person listening to music" />
-          </div>
-          <div>
-            <p>
-              Report for <span>Jeremy Robson</span>
+            <p className={styles.profile_report}>
+              Report for{" "}
+              <span className={styles.profile_name}>Jeremy Robson</span>
             </p>
           </div>
         </div>
-        <div>
-          <p>
-            <Button>Daily</Button>
-          </p>
-          <p>
-            <Button>Weekly</Button>
-          </p>
-          <p>
-            <Button> Monthly</Button>
-          </p>
+        <div className={styles.profile_report_container}>
+          <Button style={styles.profile_buttons}>Daily</Button>
+          <Button style={styles.profile_buttons}>Weekly</Button>
+          <Button style={styles.profile_buttons}>Monthly</Button>
         </div>
       </div>
     </>
