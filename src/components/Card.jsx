6a29221336e8d84274e1af54108ecc17dae0a649
Mayor Creator <paulaboagye@outlook.com>
2024-12-14 16:07:14 -0500

    add change background color function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,40 @@
+import { Information } from "./Information.jsx";
 import ellipsisImage from "../assets/images/icon-ellipsis.svg";
+import styles from "./Card.module.css";
 
 export function Card({ data }) {
   const weeklyData = data.timeframes.weekly;
 
+  const changeBackGround = (title) => {
+    switch (title) {
+      case "Work":
+        return styles.work;
+      case "Play":
+        return styles.play;
+      case "Study":
+        return styles.study;
+      case "Exercise":
+        return styles.exercise;
+      case "Social":
+        return styles.social;
+      case "Self Care":
+        return styles.self_care;
+      default:
+        return "";
+    }
+  };
+
   return (
     <>
       <div>
-        <div>
-          <div>
-            <p>{data.title}</p>
+        <div
+          className={`${styles.card_container} ${changeBackGround(data.title)}`}
+        >
+          <div className={styles.title_container}>
+            <p className={styles.title}>{data.title}</p>
             <img src={ellipsisImage} alt="ellipsis icon" />
           </div>
-          <div>
-            {Object.values(weeklyData).map((values, index) => (
-              <p key={index}>{values}</p>
-            ))}
-          </div>
+          <Information infoData={weeklyData} />
         </div>
       </div>
     </>
